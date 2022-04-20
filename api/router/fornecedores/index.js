17f68de1c98b8cb8358b6693a1c69315b4932da4
isabel.sleigman <sleimusic@gmail.com>
2022-04-20 18:05:19 -0300

    alterações em rota

--- api/router/fornecedores/index.js
+++ api/router/fornecedores/index.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const TabelaFornecedor = require('./TabelaFornecedor');
+const Fornecedor = require('./Fornecedor')
 
-router.use('/', async (requisicao, resposta) => {
+router.get('/', async (requisicao, resposta) => {
     const resultado = await TabelaFornecedor.listar()
     resposta.send(
         JSON.stringify(resultado)
     )
+})
+
+router.post('/', async (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body;
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar();
+    resposta.send(JSON.stringify(fornecedor))
 })
 
 module.exports = router
