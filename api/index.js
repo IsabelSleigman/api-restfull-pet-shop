422deb2be4899de0e750b240c719552d99a1ee29
isabel.sleigman <sleimusic@gmail.com>
2022-04-25 12:01:27 -0300

    implementando erros

--- api/index.js
+++ api/index.js
@@ -3,23 +3,30 @@ const app = express();
 const bodyParser = require('body-parser');
 const config = require('config');
 const NaoEncontrado = require('./erros/NaoEncontrado');
+const CampoInvalido = require('./erros/CampoInvalido');
+const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos');
 
 app.use(bodyParser.json())
 
 const router = require('./router/fornecedores')
+
 app.use('/api/fornecedores', router)
 
 app.use((erro, requisicao, resposta, proximo) => {
+    let status = 500
+
     if (erro instanceof NaoEncontrado) {
-        resposta.status(404)
+        status = 404
     }
-    else {
-        resposta.status(400)
+    if (erro instanceof CampoInvalido || erro instanceof DadosNaoFornecidos) {
+        status = 400
     }
+
+    resposta.status(status)
     resposta.send(
         JSON.stringify({
-            mensagem: error.message,
-            id: error.idErro
+            mensagem: erro.message,
+            id: erro.idErro
         })
     )
 })
